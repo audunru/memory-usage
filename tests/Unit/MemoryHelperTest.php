b50abcc603a3bb459527e8812a58c1104501edba
Audun Rundberg <audun@rundberg.no>
2022-01-16 18:25:49 +0100

    test: memoryhelper unit test

---
+++ tests/Unit/MemoryHelperTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace audunru\MemoryUsage\Tests\Feature;
+
+use audunru\MemoryUsage\Helpers\MemoryHelper;
+use audunru\MemoryUsage\Tests\TestCase;
+
+class MemoryHelperTest extends TestCase
+{
+    public function testItReturnsMemoryUsage()
+    {
+        $memoryHelper = new MemoryHelper();
+
+        $result = $memoryHelper->getPeakUsage();
+
+        $this->assertIsFloat($result);
+        $this->assertGreaterThan(0, $result);
+    }
+}
