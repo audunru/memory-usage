0f0c3c12556273a47167ed4cdfa5ec69fe7447e6
Audun Rundberg <audun@rundberg.no>
2022-11-27 15:13:01 +0100

    feat: response can include header with memory usage

--- src/Listeners/LogMemoryUsage.php
+++ src/Listeners/LogMemoryUsage.php
@@ -5,6 +5,7 @@ namespace audunru\MemoryUsage\Listeners;
 use audunru\MemoryUsage\Helpers\MemoryHelper;
 use Illuminate\Foundation\Http\Events\RequestHandled;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 
 class LogMemoryUsage
@@ -32,6 +33,15 @@ class LogMemoryUsage
                 $level = Arr::get($options, 'level', 'warning');
 
                 Log::channel($channel)->log($level, sprintf('Maximum memory %01.2f MiB used during request for %s is greater than limit of %01.2f MiB', $peakUsage, $event->request->getPathInfo(), $limit));
+            }
+
+            $environments = Arr::get($options, 'header.environments', []);
+            $headerIsEnabledInEnvironment = App::environment($environments);
+
+            if ($event->request->is($patterns) && $headerIsEnabledInEnvironment) {
+                $headerName = config('memory-usage.header_name', 'memory-usage');
+
+                $event->response->headers->set($headerName, $peakUsage);
             }
         }
     }
